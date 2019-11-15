0a731c20584af908dbbb9a3e59673a079c971992
Nautica <49691266+singhpk1@users.noreply.github.com>
2019-11-14 19:13:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/khan-academy-resources/',
+    prefix: '/proxy/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
