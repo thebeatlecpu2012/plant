e283bdbb7e10dc30ed90a174aeb60b207a398e55
M1k3L1m3 <dennissheynkerman@gmail.com>
2019-11-15 15:53:34 -0600

    owo

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/***************
- * node-unblocker: Web Proxy for evading firewalls and content filters,
- * similar to CGIProxy or PHProxy
- *
- *
- * This project is hosted on github:  https://github.com/nfriedly/nodeunblocker.com
- *
- * By Nathan Friedly - http://nfriedly.com
- * Released under the terms of the Affero GPL v3
- */
-
 var url = require('url');
 var querystring = require('querystring');
 var express = require('express');
@@ -41,7 +30,7 @@ function addGa(html) {
 function googleAnalyticsMiddleware(data) {
     if (data.contentType == 'text/html') {
 
-        // https://nodejs.org/api/stream.html#stream_transform
+       
         data.stream = data.stream.pipe(new Transform({
             decodeStrings: false,
             transform: function(chunk, encoding, next) {
@@ -53,7 +42,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/proxy/',
+    prefix: '/textbooks/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
@@ -61,19 +50,19 @@ var unblockerConfig = {
 
 
 
-// this line must appear before any express.static calls (or anything else that sends responses)
+
 app.use(unblocker(unblockerConfig));
 
-// serve up static files *after* the proxy is run
+
 app.use('/', express.static(__dirname + '/public'));
 
-// this is for users who's form actually submitted due to JS being disabled or whatever
+
 app.get("/no-js", function(req, res) {
-    // grab the "url" parameter from the querystring
+  
     var site = querystring.parse(url.parse(req.url).query).url;
-    // and redirect the user to /proxy/url
+  
     res.redirect(unblockerConfig.prefix + site);
 });
 
-// for compatibility with gatlin and other servers, export the app rather than passing it directly to http.createServer
+
 module.exports = app;
