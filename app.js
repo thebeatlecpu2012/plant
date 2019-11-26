86eb7818a41777e5fde581e24598322874c9ae80
MikeLime <36614258+M1k3L1m3@users.noreply.github.com>
2019-11-26 17:56:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/khan-academy/',
+    prefix: '/textbooks/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
